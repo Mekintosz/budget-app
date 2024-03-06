3cb7a3b5865d3622ebff5f1e990902675133a8ca
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-03-06 20:47:09 +0100

    Create BudgetCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
@@ -10,14 +11,20 @@ function App() {
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
-      <div>
+      <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fill, minimax(300px, 1fr))",
           gap: "1rem",
           alignItems: "flex-start",
         }}
-        <BudgetCard></BudgetCard>
+      >
+        <BudgetCard
+          name="Entertainment"
+          gray
+          amount={800}
+          max={1000}
+        ></BudgetCard>
       </div>
     </Container>
   );
